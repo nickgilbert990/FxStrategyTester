a4f28f7da25b0c3f4d996563ecd065234ad00f50
Nick Gilbert <nickgilbert990@gmail.com>
2019-02-27 15:52:41 +0000

    Added NUnit and Moq references with a dummy unit test

---
+++ UFxStrategyTeterUnitTestProject/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using cAlgo.API.Internals;
+using Moq;
+using NUnit.Framework;
+
+namespace UFxStrategyTeterUnitTestProject
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Mock<IIndicator> mockIndicator = new Mock<IIndicator>();
+        }
+    }
+}
